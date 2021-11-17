60bd1237172c0fe4869f8f0185d8d4cfcb11fa97
Andre Gabriel Foo <andreg.foo@gmail.com>
2021-11-17 20:12:33 +0800

    overrided gitignore & package-lock

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,9 +1,7 @@
 import { createStore } from "vuex";
 
 const store = createStore({
-	state: {},
-	mutations: {},
-	actions: {}
+	modules: {}
 });
 
 export default store;
--- server/app.py
+++ server/app.py
@@ -9,3 +9,9 @@ CORS(app)
 
 app.config["MONGO_URI"] = os.getenv("MONGO_URI")
 mongo = PyMongo(app)
+
+
+@app.route('/api/db')
+def api():
+    data_set = mongo.db.names.find()
+    return jsonify([data for data_set in data_set])
