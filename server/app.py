42a1f861b4b516825f40d131a8b7ede03e99ba2a
Andre Gabriel Foo <andreg.foo@gmail.com>
2021-12-27 22:27:19 +0800

    cleaned up app.py

--- server/app.py
+++ server/app.py
@@ -5,20 +5,3 @@ import os
 
 app = Flask(__name__)
 CORS(app)
-
-
-class Product:
-    def __init__(self, merchant_id, name, category, price, description, image):
-        self.name, self.category, self.price, self.description, self.image = name, category, price, description, image
-        self.__id = f'{merchant_id}-uuid4()'
-
-    def get_id(self): return self.__id
-
-    def __str__(self):
-        return f"{self.name} under {self.category} costs {self.price}\n{self.description}"
-
-
-@app.route('/api/db')
-def api():
-    data_set = mongo.db.names.find()
-    return jsonify([data for data_set in data_set])
